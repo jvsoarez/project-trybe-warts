e690ae455aef950bf3402450b19ebfe8af095ab9
jvsoarez <joaovictorcpsa@gmail.com>
2022-11-13 19:30:07 -0300

    feat: photo and simple validation for login | Requisites 1, 2 and 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginInputEmail = document.getElementById('login-input-email');
+const loginInputPass = document.getElementById('login-input-pass');
+const loginButton = document.getElementById('login-button');
+
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('OI');
+  if (loginInputEmail.value === 'tryber@teste.com' && loginInputPass.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
