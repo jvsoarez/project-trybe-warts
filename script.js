1cfac278f638b582349a19c359ce0b35fd711fc1
jvsoarez <joaovictorcpsa@gmail.com>
2022-11-18 18:16:36 -0300

    fix html estructure and style// Feat: function that replace input values to text in screen

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ textarea.addEventListener('input', () => {
   const num = textarea.value.length;
   counter.innerText = 500 - num;
 });
+
+const sectionNameAndLastname = document.querySelector('.form-input-name-container');
+const firstContainer = document.querySelector('#first-container');
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+
+function replaceInputsNameWithText() {
+  const createParagraph = document.createElement('p');
+  createParagraph.innerText = `Nome: ${inputName.value} ${inputLastName.value}`;
+  firstContainer.removeChild(sectionNameAndLastname);
+  firstContainer.appendChild(createParagraph);
+}
+
+const sectionHouse = document.querySelector('.form-input-house-container');
+const inputEmail = document.querySelector('#input-email');
+const selectHouse = document.querySelector('#house');
+
+function replaceInputsEmailAndSelectWithText() {
+  const createParagraph = document.createElement('p');
+  const createParagraphTwo = document.createElement('p');
+  createParagraph.innerText = `Email: ${inputEmail.value}`;
+  createParagraphTwo.innerText = `Casa: ${selectHouse.value}`;
+  firstContainer.removeChild(sectionHouse);
+  firstContainer.appendChild(createParagraph);
+  firstContainer.appendChild(createParagraphTwo);
+}
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  replaceInputsNameWithText();
+  replaceInputsEmailAndSelectWithText();
+});
