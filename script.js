c853d57fc039686268badf38ce087984d381de98
jvsoarez <joaovictorcpsa@gmail.com>
2022-11-21 15:34:26 -0300

    fix: forms style and position

--- script.js
+++ script.js
@@ -16,8 +16,10 @@ const agreementCheckbox = document.querySelector('#agreement');
 
 agreementCheckbox.addEventListener('change', () => {
   submitBtn.disabled = true;
+  submitBtn.style.backgroundColor = 'rgb(143, 143, 144';
   if (agreementCheckbox.checked === true) {
     submitBtn.disabled = false;
+    submitBtn.style.backgroundColor = 'rgb(80, 26, 124)';
   }
 });
 
@@ -55,8 +57,52 @@ function replaceInputsEmailAndSelectWithText() {
   firstContainer.appendChild(createParagraphTwo);
 }
 
+const radioInputsFamily = document.getElementsByClassName('family');
+const secondContainer = document.querySelector('#second-container');
+const familyContainer = document.querySelector('#family-container');
+
+function replaceInputsFamily() {
+  const createParagraph = document.createElement('p');
+  for (let i = 0; i < radioInputsFamily.length; i += 1) {
+    if (radioInputsFamily[i].checked === true) {
+      createParagraph.innerText = `Família: ${radioInputsFamily[i].value}`;
+    }
+  }
+  secondContainer.removeChild(familyContainer);
+  secondContainer.appendChild(createParagraph);
+}
+
+const contentCheckBoxInpusts = document.getElementsByClassName('subject');
+const contentContainer = document.querySelector('#checkbox-content');
+
+function supportReplaceInputsContent(text, value) {
+  let tempString = '';
+  if (text === 'Matérias: ') {
+    tempString += value;
+  } else {
+    tempString += `, ${value}`;
+  }
+  return tempString;
+}
+
+function replaceInputsContent() {
+  const createParagraph = document.createElement('p');
+  createParagraph.setAttribute('id', 'paragraph-content');
+  let text = 'Matérias: ';
+  for (let i = 0; i < contentCheckBoxInpusts.length; i += 1) {
+    if (contentCheckBoxInpusts[i].checked === true) {
+      text += supportReplaceInputsContent(text, contentCheckBoxInpusts[i].value);
+    }
+    createParagraph.innerText = text;
+  }
+  secondContainer.removeChild(contentContainer);
+  secondContainer.appendChild(createParagraph);
+}
+
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
   replaceInputsNameWithText();
   replaceInputsEmailAndSelectWithText();
+  replaceInputsFamily();
+  replaceInputsContent();
 });
